78b5fc431a9ffcb48b483c395f1a7c50229881d0
@frychicken <39238400+frychicken@users.noreply.github.com>
2018-10-20 15:09:59 -0400

    Create Nani.java

---
+++ Nani.java
@@ -0,0 +1,15 @@
+public class Nani {
+  static Frychicken fry = new Frychicken();
+   public void Nihaoma() throws Exception {
+	   int b = 10;   
+	   b = Dog_.cat_();
+	   int i = 1;
+	   while (i <= b) {
+	   Execut exec = new Execut(); 
+	   exec.run();
+	   System.out.println("lap: "+ i++);
+	   }
+	   if (fry.ChickenNotFood() != 0) {  System.out.println("Ok, doing it"); Nihaoma(); }
+	   else return;
+   }
+}
