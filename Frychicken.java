86b6673df38d02b26062350e4732742c59fae3dd
@frychicken <39238400+frychicken@users.noreply.github.com>
2018-10-21 20:28:56 -0400

    neat + change alert to "confirm"

--- Frychicken.java
+++ Frychicken.java
@@ -1,13 +1,13 @@
 import java.awt.Component;
 import javax.swing.JOptionPane;
 public class Frychicken {
-  public int ChickenNotFood() throws Exception {
-	  int i = isChickenFood("Are your sure you want to exit?");
-	  return i;
-  }
-  public static int isChickenFood(String theMessage) {
-	    int result = JOptionPane.showConfirmDialog((Component) null, theMessage,
-	        "Are you sure?", JOptionPane.OK_CANCEL_OPTION);
-	    return result;
-	  }
+	public int ChickenNotFood() throws Exception {
+		int i = isChickenFood("Are your sure you want to exit?");
+		return i;
+	}
+	public static int isChickenFood(String theMessage) {
+		int result = JOptionPane.showConfirmDialog((Component) null, theMessage,
+				"Confirm", JOptionPane.OK_CANCEL_OPTION);
+		return result;
+	}
 }
