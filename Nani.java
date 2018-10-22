99ef23f80fe45d699165cecec4c59d9456336c93
@frychicken <39238400+frychicken@users.noreply.github.com>
2018-10-21 20:29:41 -0400

    neat + show how many labs you entered

--- Nani.java
+++ Nani.java
@@ -1,15 +1,16 @@
 public class Nani {
-  static Frychicken fry = new Frychicken();
-   public void Nihaoma() throws Exception {
-	   int b = 10;   
-	   b = Dog_.cat_();
-	   int i = 1;
-	   while (i <= b) {
-	   Execut exec = new Execut(); 
-	   exec.run();
-	   System.out.println("lap: "+ i++);
-	   }
-	   if (fry.ChickenNotFood() != 0) {  System.out.println("Ok, doing it"); Nihaoma(); }
-	   else return;
-   }
+	static Frychicken fry = new Frychicken();
+	public void Nihaoma() throws Exception {
+		int b = 10;   
+		b = Dog_.cat_();
+		System.out.println("Labs: "+b);
+		int i = 1;
+		while (i <= b) {
+			Execut exec = new Execut(); 
+			exec.run();
+			System.out.println("lap: "+ i++);
+		}
+		if (fry.ChickenNotFood() != 0) {  System.out.println("Ok, doing it"); Nihaoma(); }
+		else return;
+	}
 }
