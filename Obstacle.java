e41332bf4f5cf5cbd7f2fb947438fb81a8d0f39e
@frychicken <39238400+frychicken@users.noreply.github.com>
2018-10-21 20:30:20 -0400

    look more neat

--- Obstacle.java
+++ Obstacle.java
@@ -1,19 +1,20 @@
 public class Obstacle {
 	public static void main(String[] args) throws Exception {
-	  String f = "Avoiding objects simulator by @frychicken";
-	  for (int i =0; i<f.length(); i++) {
-		  String c = f.substring(i, i+1);
-		  System.out.print(c);
-		  try {
-			  Thread.sleep(90); 
-		   } catch (Exception e) {
-			  e.printStackTrace();
-		  }
-      } 
-	  System.out.println();
-	  Nani nina = new Nani();
-	  nina.Nihaoma();
-	  System.out.println("All Done!");
-	  System.exit(0);
+		String f = "Avoiding objects simulator by @frychicken";
+		for (int i =0; i<f.length(); i++) {
+			String c = f.substring(i, i+1);
+			System.out.print(c);
+			try {
+				Thread.sleep(90); 
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} 
+		System.out.println();
+		Nani nina = new Nani();
+		nina.Nihaoma();
+		System.out.println("All Done!");
+		System.exit(0);
 	}
 }
+
