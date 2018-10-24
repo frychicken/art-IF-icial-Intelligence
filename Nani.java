463b411cc0d019120b3261074aa2891ffac4155b
@frychicken <39238400+frychicken@users.noreply.github.com>
2018-10-24 18:36:00 -0400

    printf (obstacles user entered in)

--- Nani.java
+++ Nani.java
@@ -1,12 +1,21 @@
+
 public class Nani {
 	static Frychicken fry = new Frychicken();
 	public void Nihaoma() throws Exception {
 		int b = 10;   
 		b = Dog_.cat_();
-		System.out.println("Labs: "+ b);
+		System.out.println("Labs: "+b);
 		int i = 1;
+		int userin = 4;
+		while (userin >3 || userin < 0) {
+			userin = Dog_.fudgingcat_();
+			if (userin >3 || userin < 0)
+				fry.ChickenIsNotFood();
+		}
+		System.out.println("Obstacles: "+ userin);
 		while (i <= b) {
-			Execut exec = new Execut(); 
+			Execut exec = new Execut(userin); 
+			exec.assin();
 			exec.run();
 			System.out.println("lap: "+ i++);
 		}
