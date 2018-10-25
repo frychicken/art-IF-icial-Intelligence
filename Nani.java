443eee7098648f5da2bf4f5183defc4f37c46a40
@frychicken <39238400+frychicken@users.noreply.github.com>
2018-10-24 22:13:23 -0400

    improve pintf()

--- Nani.java
+++ Nani.java
@@ -4,7 +4,7 @@ public class Nani {
 	public void Nihaoma() throws Exception {
 		int b = 10;   
 		b = Dog_.cat_();
-		System.out.println("Labs: "+b);
+		System.out.println("Lab(s): "+b);
 		int i = 1;
 		int userin = 4;
 		while (userin >3 || userin < 0) {
@@ -12,7 +12,7 @@ public class Nani {
 			if (userin >3 || userin < 0)
 				fry.ChickenIsNotFood();
 		}
-		System.out.println("Obstacles: "+ userin);
+		System.out.println("Obstacle(s): "+ userin);
 		while (i <= b) {
 			Execut exec = new Execut(userin); 
 			exec.assin();
