9c7915f8b9eceff418daae8acc2175aec9ef7b25
ᗺ0B <39238400+frychicken@users.noreply.github.com>
2018-10-26 20:16:37 -0400

    Loading screen

--- Obstacle.java
+++ Obstacle.java
@@ -1,15 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
 public class Obstacle {
 	public static void main(String[] args) throws Exception {
+		Loading loadd = new Loading();
+		JFrame frame = new JFrame("Loading"); 
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+		frame.getContentPane().add(BorderLayout.CENTER, loadd);  
+		frame.setVisible(true); 
+		frame.setResizable(false);
+		frame.setSize(300, 300); 
 		String f = "Avoiding objects simulator by @frychicken";
 		for (int i =0; i<f.length(); i++) {
 			String c = f.substring(i, i+1);
 			System.out.print(c);
 			try {
+				loadd.changec();
+				loadd.repaint();
 				Thread.sleep(90); 
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		} 
+		frame.setVisible(false);
 		System.out.println();
 		Nani nina = new Nani();
 		nina.Nihaoma();
@@ -17,4 +29,3 @@ public class Obstacle {
 		System.exit(0);
 	}
 }
-
