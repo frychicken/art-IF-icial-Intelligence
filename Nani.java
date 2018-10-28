dec47d187de6e46eac7e6e0f8b56529b38200cfe
ᗺ0B <39238400+frychicken@users.noreply.github.com>
2018-10-27 23:17:52 -0400

    Update Nani.java

--- Nani.java
+++ Nani.java
@@ -1,33 +1,35 @@
-
+import java.awt.Color;
 public class Nani {
 	static Frychicken fry = new Frychicken();
 	public void Nihaoma() throws Exception {
 		int b = 10; 
 		String reduc = Dog_.cat_();
+		Color obstacle = new Color(Integer.valueOf(reduc.substring(reduc.indexOf("*")+1,reduc.indexOf("!"))),Integer.valueOf(reduc.substring(reduc.indexOf("!")+1,reduc.indexOf("@"))),Integer.valueOf(reduc.substring(reduc.indexOf("@")+1,reduc.indexOf("#"))));
+		Color object = new Color(Integer.valueOf(reduc.substring(reduc.indexOf("#")+1,reduc.indexOf("$"))),Integer.valueOf(reduc.substring(reduc.indexOf("$")+1,reduc.indexOf("%"))),Integer.valueOf(reduc.substring(reduc.indexOf("%")+1)));
 		int tess = reduc.indexOf(".");
 		b = Integer.valueOf(reduc.substring(0,tess));
 		System.out.println("Lab(s): "+b);
 		int i = 1;
-		int userin = Integer.valueOf(reduc.substring(tess+1));
+		int userin = Integer.valueOf(reduc.substring(tess+1, reduc.indexOf(",")));
 		if (userin > 3 || userin<=0)
-			while (userin >3 || userin <=0) {
-				userin = Integer.valueOf(Dog_.cat_().substring(tess+1));
+			while (userin >3 || userin <=0) { 
+				userin = Integer.valueOf(Dog_.cat_().substring(tess+1, reduc.indexOf(",")));
 				if (userin >3 || userin <=0)
 					fry.ChickenIsNotFood();
 			}
 		boolean debugg = true;
 		int n = -1;
-		int st = fry.FriedChicken();
+		int st = Integer.valueOf(reduc.substring(reduc.indexOf(",")+1,reduc.indexOf("*")));
 		System.out.println("Obstacle(s): "+ userin);
 		while (i <= b) {
-			Execut exec = new Execut(userin); 
-			if (st != 0) {
+			Execut exec = new Execut(userin, object, obstacle,i,b); 
+			if (st != 1) {
 				debugg = false;
 				exec.assin(debugg);
 			}
 			else { debugg = true; exec.assin(debugg);}
 			if (n == -1)
-				System.out.println("Debug mode = " +debugg);
+			System.out.println("Debug mode = " +debugg);
 			exec.run();
 			System.out.println("lap: "+ i++);
 			n++;
