17bf64659ef18c9d4e544529b555b326b1214024
ᗺ0B <39238400+frychicken@users.noreply.github.com>
2018-10-30 21:04:30 -0400

    Update Nani.java

--- Nani.java
+++ Nani.java
@@ -1,9 +1,11 @@
 import java.awt.Color;
+
 public class Nani {
 	static Frychicken fry = new Frychicken();
 	public void Nihaoma() throws Exception {
 		int b = 10; 
-		String reduc = Dog_.cat_();
+		Dog_ doge = new Dog_();
+		String reduc = doge.cat_();
 		Color obstacle = new Color(Integer.valueOf(reduc.substring(reduc.indexOf("*")+1,reduc.indexOf("!"))),Integer.valueOf(reduc.substring(reduc.indexOf("!")+1,reduc.indexOf("@"))),Integer.valueOf(reduc.substring(reduc.indexOf("@")+1,reduc.indexOf("#"))));
 		Color object = new Color(Integer.valueOf(reduc.substring(reduc.indexOf("#")+1,reduc.indexOf("$"))),Integer.valueOf(reduc.substring(reduc.indexOf("$")+1,reduc.indexOf("%"))),Integer.valueOf(reduc.substring(reduc.indexOf("%")+1)));
 		int tess = reduc.indexOf(".");
@@ -13,7 +15,7 @@ public class Nani {
 		int userin = Integer.valueOf(reduc.substring(tess+1, reduc.indexOf(",")));
 		if (userin > 3 || userin<=0)
 			while (userin >3 || userin <=0) { 
-				userin = Integer.valueOf(Dog_.cat_().substring(tess+1, reduc.indexOf(",")));
+				userin = Integer.valueOf(doge.cat_().substring(tess+1, reduc.indexOf(",")));
 				if (userin >3 || userin <=0)
 					fry.ChickenIsNotFood();
 			}
