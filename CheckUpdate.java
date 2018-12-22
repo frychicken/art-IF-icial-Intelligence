3bcc27c6fd82ae0d11aa9473da5a1077041a76cc
ᗺ0B <39238400+frychicken@users.noreply.github.com>
2018-12-21 20:38:16 -0500

    Update CheckUpdate.java

--- CheckUpdate.java
+++ CheckUpdate.java
@@ -1,21 +1,14 @@
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
-
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 public class CheckUpdate {
 	private boolean stillqm = true;
- @SuppressWarnings("resource")
-public void checkup() throws Exception {
+ public void checkup() throws Exception {
 	 String todis = "Checking for updates";
 		JFrame frame = new JFrame("Checking update"); 
 		DrawCheck drc= new DrawCheck(frame);
@@ -33,32 +26,7 @@ public void checkup() throws Exception {
 			e.printStackTrace();
 		}
 		
-	 String cheee = null; 
-	 try {
-	 cheee = getClass().getResource("version.txt").toString();
-	 cheee = cheee.substring(cheee.indexOf(":")+1);
-	 } catch (Exception e) {
-		 System.out.println("Error checking update: Missing dependencie(s)");
-		 todis = "Error checking update: Missing dependencie(s)";
-			try {
-				drc.changec(todis);
-				drc.repaint();
-				Thread.sleep(1000);
-			} catch (Exception er) {
-				er.printStackTrace();
-			}
-			frame.setVisible(false);  
-			stillqm = false;
-			return;
-	 }
-	 File checkifex = new File(cheee);
-		if(checkifex.exists() && !checkifex.isDirectory()) {
-			BufferedReader br2 = new BufferedReader(new FileReader(cheee)); 
-			String st2; 
-			StringBuilder fromweb = new StringBuilder();
-			while ((st2 = br2.readLine()) != null) { 
-				fromweb.append(st2);
-			}
+	 int cheee = drc.getVersion(); 
 			System.out.println("Checking for updates");
 			BufferedReader br = null;
 			StringBuilder fromcom = new StringBuilder();
@@ -88,7 +56,7 @@ public void checkup() throws Exception {
 				}
 			} 
 			int c =Integer.valueOf(fromcom.toString());
-			int d = Integer.valueOf(fromweb.toString());
+			int d = cheee;
 			if (c > d) {
 				 todis = "New version is available";
 					try {
@@ -100,24 +68,6 @@ public void checkup() throws Exception {
 					}
 				if (JOptionPane.showConfirmDialog((Component) null, "New version is available Do you want to update?\n new version:" +c+" your version: "+d,
 						"Confirm", JOptionPane.YES_NO_OPTION) ==0) {
-					BufferedWriter bw = null;
-					FileWriter fw = null;
-					try {
-						fw = new FileWriter(cheee);
-						bw = new BufferedWriter(fw);
-						bw.write(fromcom.toString());
-					} catch (IOException e) {
-						e.printStackTrace();
-					} finally {
-						try {
-							if (bw != null)
-								bw.close();
-							if (fw != null)
-								fw.close();
-						} catch (IOException ex) {
-							ex.printStackTrace();
-						}
-					}
 					 todis = "Almost done! Updating...";
 						try {
 							drc.changec(todis);
@@ -143,24 +93,11 @@ public void checkup() throws Exception {
 			else {System.out.println("No new updates");
 			JOptionPane.showConfirmDialog((Component) null, "No new updates",
 					"Update", JOptionPane.CLOSED_OPTION);
-			frame.setVisible(false); 
+					frame.setVisible(false);
 			stillqm = false;
 			}
 				
        }
-		else {
-			 todis = "Cannot check for update";
-				try {
-					drc.changec(todis);
-					drc.repaint();
-				} catch (Exception er) {
-					er.printStackTrace();
-				}
-			JOptionPane.showConfirmDialog((Component) null, "Cannot check for update\n Reason: version = null",
-					"Update", JOptionPane.CLOSED_OPTION);
-			frame.setVisible(false);  
-			stillqm = false;
-		}
+
  }
-}
 }
