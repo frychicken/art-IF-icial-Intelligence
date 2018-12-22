e45bfcc4d1f3d7d1427717c1446e4ab4f249fcfb
ᗺ0B <39238400+frychicken@users.noreply.github.com>
2018-12-21 20:40:09 -0500

    Update Execut.java

--- Execut.java
+++ Execut.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.io.IOException;
 import java.util.*;
 public class Execut  extends Component {
 	private static final long serialVersionUID = 1L;
@@ -20,6 +21,7 @@ public class Execut  extends Component {
 	int lalala;
 	int alala;
 	boolean autopilot = false;
+	Frychicken fry = new Frychicken();
 	public Execut(int userin, Color object, Color obstacle, int i, int b) {
 		this.object = object;
 		this.obstacle = obstacle;
@@ -27,7 +29,7 @@ public class Execut  extends Component {
 		alala = b;
 		this.userin = userin;
 	}
-	public void assin(boolean debugg, boolean sound, boolean autopilot) {
+	public void assin(boolean debugg, boolean sound, boolean autopilot) throws IOException {
 		this.debugg = debugg;
 		this.sound = sound;
 		this.autopilot = autopilot;
@@ -54,7 +56,10 @@ public class Execut  extends Component {
 		if (debugg) {
 			for (int i= 0; i< userin; i++){
 				System.out.println("x" + (i+1) + " " +xOb.get(i));
+				fry.writeLog("x" + (i+1) + " " +xOb.get(i));
 				System.out.println("y" + (i+1) +" " +yOb.get(i));
+				fry.writeLog("y" + (i+1) +" " +yOb.get(i));
+
 			}
 		}
 	}
@@ -69,9 +74,11 @@ public class Execut  extends Component {
 		int rand = randomness.nextInt(400)+30;
 		return rand;
 	}
+	int owenn;
+	int owenn2;
 	class Method_T extends Component {
 		private static final long serialVersionUID = 1L;
-
+		int ccc = 0;
 		public void paint(Graphics g){
 
 			for (int i =0; i < userin; i++) {
@@ -80,10 +87,28 @@ public class Execut  extends Component {
 				if ((y >= yOb.get(i) && y<= yOb.get(i) +150) && (x+123 >= xOb.get(i)  && x+123 <= xOb.get(i)+150) && debugg) {
 					g.setColor(Color.gray);
 					g.fillRect(xOb.get(i),yOb.get(i),150,150);
+					owenn = yOb.get(i);
+					owenn2 = xOb.get(i);
+					if (ccc == 0) {
+						System.out.println("Hit at x of object: "+owenn2);
+						System.out.println("Hit at y of object: "+owenn);
+						fry.writeLog("Hit at x of object: "+owenn2);
+						fry.writeLog("Hit at y of object: "+owenn);
+						ccc++;
+					}
 				}
 				else if ((y >= yOb.get(i)  && y<= yOb.get(i) +150) && (x >= xOb.get(i)  && x <= xOb.get(i)+150) && debugg) {
 					g.setColor(Color.gray);
 					g.fillRect(xOb.get(i),yOb.get(i),150,150);
+					owenn = yOb.get(i);
+					owenn2 = xOb.get(i);
+					if (ccc ==0) {
+						System.out.println("Hit at x of object: "+owenn2);
+						System.out.println("Hit at y of object: "+owenn);
+						fry.writeLog("Hit at x of object: "+owenn2);
+						fry.writeLog("Hit at y of object: "+owenn);
+						ccc++;
+					}
 				}
 			}
 			if (debugg) {
@@ -95,13 +120,19 @@ public class Execut  extends Component {
 				g.drawString(msg, 300, 750);
 				int c = 700;
 				int cc = 700;
+
 				for (int i =0; i<userin; i++) {
+					g.drawString("Hit at y of object: "+owenn, 250, 660);	
+					g.drawString("Hit at x of object :"+owenn2, 250, 675);
+					g.drawString("y object: "+y, 150, 660);	
+					g.drawString("x object: "+x, 150, 675);
 					g.drawString("x"+(i+1)+": "+xOb.get(i), 500, c+=20);
 					g.drawString("y"+(i+1)+": "+yOb.get(i), 430, cc+=20);
-					g.drawString("lab(s) run: "+lalala, 60, 720);
+					g.drawString("lab(s) ran: "+lalala, 60, 720);
 					g.drawString("Obstacle(s): "+userin, 60, 700);
 					g.drawString("lab(s): "+alala, 60, 680);	
 					g.drawString("auto-pilot: "+autopilot, 250, 690);	
+
 				}
 			}
 			g.setColor(object);
@@ -109,7 +140,7 @@ public class Execut  extends Component {
 		}
 	}
 	public void run() {
-		frame = new JFrame("AO Simulator" + " @Debug mode: "+debugg + ", sound: "+ sound+", AP: "+ autopilot); 
+		frame = new JFrame("AO Simulator" + " @Debug mode: "+debugg + ", sound: "+ sound); 
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
 		contentt = new Method_T();
 		frame.getContentPane().add(BorderLayout.CENTER, contentt);  
@@ -117,66 +148,67 @@ public class Execut  extends Component {
 		frame.setResizable(false);
 		frame.setSize(600, 800); 
 		if (autopilot) {
-		frame.addKeyListener(new KeyAdapter() {
-            public void keyPressed(KeyEvent event) {
-                moveTheTHing(event);
-            }
-        });
+			frame.addKeyListener(new KeyAdapter() {
+				public void keyPressed(KeyEvent event) {
+					moveTheTHing(event);
+				}
+			});
 		}
 		frame.setFocusable(true); 
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true); 
 		runpls();
 	}
-	
-	
+
+
 	private boolean right = false;
 	private boolean left = false;
 	private boolean up = true;
 	private boolean down = false;
 	private boolean check = true;
-	
-	   public void moveTheTHing(KeyEvent event) { 
-	          
-		      int key = event.getKeyCode(); 
-		      int key2 = event.getKeyChar();
-		      if (key == KeyEvent.VK_LEFT) {
-		    	     x-=10;
-					check = false;
-					msg = "LEFT";
-		      }
-		      else if (key == KeyEvent.VK_RIGHT) {
-		    	     x+=10;
-					check = false;
-					msg = "RIGHT";
-		      }
-		      else if (key == KeyEvent.VK_UP) {
-		    	     y-=20;
-					check = false;
-					msg = "UP";
-		      }
-		      else if (key == KeyEvent.VK_DOWN) {
-		    	     y+=20;
-					check = false;
-					msg = "DOWN";
-		      }
-		      if (key2 == 's') {
-		    	  up = false;
-		    	  msg = "Stop";
-		      }
-		      if (key2 == 'c') {
-		    	  up = true;
-		    	  msg = "Moving";
-		      }
 
-		   }
-		
-	
+	public void moveTheTHing(KeyEvent event) { 
+
+		int key = event.getKeyCode(); 
+		int key2 = event.getKeyChar();
+		if (key == KeyEvent.VK_LEFT) {
+			x-=10;
+			check = false;
+			msg = "LEFT";
+		}
+		else if (key == KeyEvent.VK_RIGHT) {
+			x+=10;
+			check = false;
+			msg = "RIGHT";
+		}
+		else if (key == KeyEvent.VK_UP) {
+			y-=20;
+			check = false;
+			msg = "UP";
+		}
+		else if (key == KeyEvent.VK_DOWN) {
+			y+=20;
+			check = false;
+			msg = "DOWN";
+		}
+		if (key2 == 's') {
+			up = false;
+			msg = "Stop";
+		}
+		if (key2 == 'c') {
+			up = true;
+			msg = "Moving";
+		}
+
+	}
+
+
 	public void runpls(){
 		while(true) {
-	
+
 			for (int i =0; i < userin; i ++) {
 				if ((y >= yOb.get(i) && y<= yOb.get(i) +150) && (x+123 >= xOb.get(i)  && x+123 <= xOb.get(i)+150) && check) {
+
 					left = true;
 					right = false;
 				}
@@ -185,31 +217,31 @@ public class Execut  extends Component {
 					right = true;
 				}
 				if (i==3){
-				if (yOb.get(userin-2)-yOb.get(userin-1)-150<= 35 &&( (xOb.get(userin-2) < xOb.get(userin-1) && xOb.get(userin-3) > xOb.get(userin-1))||(xOb.get(userin-3) < xOb.get(userin-1) && xOb.get(userin -2) > xOb.get(userin-1)))){
-				 down= true;
-				 }
+					if (yOb.get(userin-2)-yOb.get(userin-1)-150<= 35 &&( (xOb.get(userin-2) < xOb.get(userin-1) && xOb.get(userin-3) > xOb.get(userin-1))||(xOb.get(userin-3) < xOb.get(userin-1) && xOb.get(userin -2) > xOb.get(userin-1)))){
+						down= true;
+					}
 				}
 				int fff = 0;
 				if(down){
-				 y+=3;
-				 fff++;
-				 if (fff==50){
-				 right =!right;
-				 left =!left;
-				 fff=0;
-				 up=true;
-				 }
+					y+=3;
+					fff++;
+					if (fff==50){
+						right =!right;
+						left =!left;
+						fff=0;
+						up=true;
+					}
 				}
-			      
-				
+
+
 				if (left) {
 					msg = "LEFT";
 					x-=3;
 					if (y <= yOb.get(i) +150 && (x+123) < xOb.get(i)  ) { up = true; }
 					else up = false;
 					if (sound) {
-					Soundfx soundfx = new Soundfx();
-					soundfx.sound();
+						Soundfx soundfx = new Soundfx();
+						soundfx.sound();
 					}
 				}
 				if(right) {
@@ -218,8 +250,8 @@ public class Execut  extends Component {
 					if (y <= yOb.get(i) +150 && x > xOb.get(i)  ) { up = true; }
 					else up = false;
 					if (sound) {
-					Soundfx soundfx = new Soundfx();
-					soundfx.sound();
+						Soundfx soundfx = new Soundfx();
+						soundfx.sound();
 					}
 				}
 				if(up) {
@@ -229,8 +261,8 @@ public class Execut  extends Component {
 				}
 				if (y+40+50 <= 0) {
 					if (sound) {
-					Soundfx soundfx = new Soundfx();
-					soundfx.sound();
+						Soundfx soundfx = new Soundfx();
+						soundfx.sound();
 					}
 					frame.setVisible(false); 
 					return;
@@ -248,3 +280,4 @@ public class Execut  extends Component {
 		}
 	}
 }
+
